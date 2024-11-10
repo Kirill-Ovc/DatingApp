97d7b642fa8c1e4314c05c4cfd11c84c55e84e1c
Kirill-Ovc <kirill.056@gmail.com>
2024-11-10 18:21:31 +0300

    Section 10 - update service

--- API/Extensions/IdentityServiceExtensions.cs
+++ API/Extensions/IdentityServiceExtensions.cs
@@ -1,6 +1,29 @@
-﻿namespace API;
+﻿using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 
-public class IdentityServiceExtension
+namespace API.Extensions;
+
+public static class IdentityServiceExtensions
 {
+    public static IServiceCollection AddIdentityServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options => 
+                {        
+                    var key = configuration["TokenKey"];
+                #pragma warning disable CA5404 // Do not disable token validation checks
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(
+                            Encoding.UTF8.GetBytes(key)),
+                        ValidateIssuer = false,
+                        ValidateAudience = false
+                    };
+                #pragma warning restore CA5404 // Do not disable token validation checks
+                });
 
+        return services;
+    }
 }
--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -33,7 +33,11 @@ export class MembersService {
 
   updateMember(member: Member)
   {
-    return this.http.put(this.baseUrl + 'users', member);
+    return this.http.put(this.baseUrl + 'users', member)
+      .pipe(map(() => {
+        const index = this.members.indexOf(member);
+        this.members[index] = {...this.members[index], ...member};
+      }));
   }
 
   // we have interceptor
