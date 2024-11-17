57eb4d8c222cd2e25bbcfd8af203790cc4199ed4
Kirill-Ovc <kirill.056@gmail.com>
2024-11-17 15:41:35 +0300

    Add PhotoService for photo upload

---
+++ client/src/app/members/photo-editor/photo-editor.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input } from '@angular/core';
+import { Member } from 'src/app/_models/member';
+
+@Component({
+  selector: 'app-photo-editor',
+  templateUrl: './photo-editor.component.html',
+  styleUrls: ['./photo-editor.component.css']
+})
+export class PhotoEditorComponent {
+  @Input() member: Member | undefined;
+
+  ngOnInit() {
+    // Runtime check to ensure input is provided
+    if (!this.member) {
+      throw new Error('Member input is required.');
+    }
+  }
+}
